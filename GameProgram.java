8cc25329a69718924d61dd79ce98b692173e2564
namps <namrathap99@yahoo.com>
2015-09-07 13:03:11 -0700

    Runs the program

---
+++ GameProgram.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.applet.Applet;
+import javax.swing.*;
+
+public class GameProgram extends JApplet{
+        public void init(){
+        FirstPage fsp = new FirstPage();
+        setContentPane(fsp);
+        }
+}
